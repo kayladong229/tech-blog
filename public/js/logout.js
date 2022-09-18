83e2b4f2401b8af6cf92fc9e1f93771929a155ac
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-18 11:43:50 -0700

    Added initial code

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async function () {
+  const response = await fetch('/api/user/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Unable to log out.');
+  }
+};
+
+document.querySelector('#logout-link').addEventListener('click', logout);
