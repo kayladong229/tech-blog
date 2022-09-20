795bf507153b8fe3a049628831ff65ff9d9bfc1d
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-19 18:25:26 -0700

    Edited signup.js code

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,24 +2,30 @@
 const signupFormHandler = async function (event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-signup');
-  const passwordEl = document.querySelector('#password-input-signup');
+  const username = document
+    .querySelector('#username-input-signup')
+    .value.trim();
+  const password = document
+    .querySelector('#password-input-signup')
+    .value.trim();
 
-  await fetch('/api/user', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  if (username && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Unable to sign up.');
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Unable to sign up.');
+    }
   }
 };
 
