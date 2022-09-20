bd7e267a099c26ff7e7e417219f8a6754baa9851
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-19 18:37:05 -0700

    Fixed login.js code

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,26 @@
 const loginFormHandler = async function (event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-login');
-  const passwordEl = document.querySelector('#password-input-login');
+  const username = document.querySelector('#username-input-login').value.trim();
+  const password = document.querySelector('#password-input-login').value.trim();
 
-  const response = await fetch('/api/user/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Unable to log in.');
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Unable to log in.');
+    }
   }
 };
 
