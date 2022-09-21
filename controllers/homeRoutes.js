858f749cb6ec02defad5de4ea7658660d36056dc
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-20 17:40:01 -0700

    Updated homeRoutes.js code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,9 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Render serialized data
-    res.render('all-posts', { posts });
+    res.render('all-posts', {
+      posts,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -46,7 +48,9 @@ router.get('/post/:id', async (req, res) => {
     // Serialize post data
     const post = postData.get({ plain: true });
 
-    res.render('single-post', { post });
+    res.render('single-post', {
+      post,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
