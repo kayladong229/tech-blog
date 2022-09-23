2f8afa682220471a0cc613567bb02916625178d6
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-22 17:03:45 -0700

    Fixed code to properly display post date

--- models/Comment.js
+++ models/Comment.js
@@ -17,6 +17,11 @@ Comment.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    date_created: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
